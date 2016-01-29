ad558827f000e3773daa6e4883e2c4ce28e605ec
kevguy <kevlai@uw.edu>
2016-01-29 09:25:46 +0800

    Add config.js

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db: process.env.db || 'localhost',
+  clientSecret: process.env.clientSecret || 'fbfe8923eda1464eb2626a905f9ce384',
+  tokenSecret: process.env.tokenSecret || 'pick a hard to guess string'
+
+};
