d631a442ab59e28053e0fcc7958b1f7017c0592e
kevguy <kevlai@uw.edu>
2016-01-30 05:03:02 +0800

    Login Route

--- server/server.js
+++ server/server.js
@@ -64,6 +64,26 @@ function isAuthenticated(req, res, next) {
   })
 }
 
+app.post('/auth/login', function(req, res) {
+  User.findOne({ email: req.body.email }, '+password', function(err, user) {
+    if (!user) {
+      return res.status(401).send({ message: { email: 'Incorrect email' } });
+    }
+
+    bcrypt.compare(req.body.password, user.password, function(err, isMatch) {
+      if (!isMatch) {
+        return res.status(401).send({ message: { password: 'Incorrect password' } });
+      }
+
+      user = user.toObject();
+      delete user.password;
+
+      var token = createToken(user);
+      res.send({ token: token, user: user });
+    });
+  });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
